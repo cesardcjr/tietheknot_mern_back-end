ef9337762da4c5ff4881e64cef64f87b0f3fb63b
cesardcjr <engineer.cesardcjr@gmail.com>
2026-04-05 17:25:08 +0800

    updated the cors in server.js

--- src/server.js
+++ src/server.js
@@ -1,38 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 connectDB();
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  'http://localhost:5173',
-  'http://localhost:3000',
+  "http://localhost:5173",
+  "http://localhost:3000",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
+app.options("*", cors());
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.json({ message: 'TieTheKnot PH API is running' }));
+app.get("/", (req, res) =>
+  res.json({ message: "TieTheKnot PH API is running" }),
+);
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/data', require('./routes/data'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/data", require("./routes/data"));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: 'Internal server error' });
+  res.status(500).json({ message: "Internal server error" });
 });
 
 const PORT = process.env.PORT || 5000;
